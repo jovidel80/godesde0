d3582bf847f8c3a399bd15557805dd06c802927b
Jose Oliveros <joliverosc@bancoripley.com>
2023-05-12 23:17:26 -0400

    primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jovidel80/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/jovidel80/godesde0/variables"
+)
+
+func main() {
+	variables.MostrarEnteros()
+}
